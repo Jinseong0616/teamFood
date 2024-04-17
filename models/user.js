89201f798b9ca6263b1045bb90919dc17ac5d9a2
kihongDev <ssorru0623@gmail.com>
2024-04-17 11:16:02 +0900

    맛집 first

---
+++ models/user.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    userId: {
+      type : DataTypes.STRING,
+      primaryKey : true
+    },
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    address: DataTypes.STRING,
+    phone: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    timestamps: false, // createdAt, updatedAt 컬럼 생성 방지
+    createdAt: 'createdAt', // 자동 생성되는 createdAt 필드명 정의
+    updatedAt: 'updatedAt',
+    id : false
+  });
+  return User;
+};
---
+++ server.js
@@ -0,0 +1,130 @@
+// 1. 모듈 - require
+const express = require('express')
+const sequelize = require('sequelize')
+const app = express()
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+
+// db
+const db = require('./models')
+const {User} = db
+
+// 포트
+
+const port = 3000
+
+// 2. use, set 
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+// 3. listen
+app.listen(port, ()=>{
+  console.log('접속 성공! - http://localhost:'+ port)
+})
+
+app.use(passport.initialize())
+
+app.use(session({
+    secret: '1234',
+    resave : false,
+    saveUninitialized : false,
+    cookie : {maxAge: 60*60*1000}
+}))
+
+app.use(passport.session())
+
+passport.use(new LocalStrategy(async (username, pw, done)=>{
+  let result = await User.findOne({where : {userId : username}})
+  
+  if(!result){
+    return done(null, false, {message : 'ID가 DB에 없음'})
+  }
+  else if(result.password != pw){
+    return done(null, false, {message : '비밀번호 불일치'})
+  }
+  else{
+    return done(null, result)
+  }
+
+}))
+
+app.get('/login', (req,res)=>{
+  res.render('login.ejs')
+})
+
+app.get('/list', (req,res)=>{
+  const userId = req.isAuthenticated() ? req.user.userId : false
+  res.render('list.ejs', {userId})
+})
+
+app.post('/login',(req,res)=>{
+  passport.authenticate('local', (error, user, info)=>{
+      
+      console.log(error)
+      console.log(user)
+      console.log(info)
+      if(error) return res.status(500).json(error) // 인증 과정에서 오류
+      if(!user) return res.send('로그인 실패')
+
+      req.logIn(user, (err)=>{
+          if(err) return next(err)
+           req.session.userId = user.userId;
+           return res.redirect('/list')   
+      })
+  })(req,res)
+})
+
+passport.serializeUser((user, done) =>{
+  process.nextTick(()=>{
+    // 세션 생성할 때 비밀번호가 들어가지 않음, user의  id와 이름만 알려주면 이 정보를 기록해주고
+    // 유효기간은 cookie에 생성해준 기간으로 자동으로 생성해서 기록해줌.
+    done(null, {userId : user.userId})
+  })
+})
+
+passport.deserializeUser(async(user, done) =>{
+  let result = await User.findOne({where : {userId : user.userId}})
+  delete result.password  // 객체에서 파라미터 지움. password 파라미터 필요없어서 지움
+  
+    const newUserInfo={
+      userId : result.userId,
+      
+    }
+  process.nextTick(()=>{
+    return done(null, newUserInfo)
+  })
+})
+
+
+
+
+
+app.get('/join', async function(req,res){
+  res.render('join.ejs')
+})
+
+
+// 회원가입
+app.post('/join', async function(req,res){
+  const newMember = req.body
+  console.log(newMember)
+  try{
+      const member = await User.findOne({where : {userId : newMember.userId}})
+      if(member){
+          return res.send('중복입니다.')
+      }
+      const addMember = await User.create(newMember)
+      res.redirect('/login')
+  }catch(error){
+      console.log('검색 중 오류 발생', error)
+      res.status(500).send("서버 오류 발생");
+  }
+}) 
+
+
+
+
