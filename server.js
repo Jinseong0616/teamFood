7e566085b2692f152bb1aff30b77295ff678902b
jinseong <zmdktkwjs@naver.com>
2024-04-29 14:15:20 +0900

    0429:14

--- server.js
+++ server.js
@@ -85,8 +85,12 @@ passport.use(
 
 // 메인 페이지
 app.get("/", async (req, res) => {
+  
   const userId = req.isAuthenticated() ? req.user.userId : false;
+  
   const user = await User.findOne({where : {userId}})
+  
+  console.log(user.name)
   res.render("index.ejs", { name : user.name , userId });
 });
 
@@ -436,13 +440,8 @@ app.get("/search", async function (req, res) {
         ],
       });
     }
-<<<<<<< HEAD
-    // console.log(shops[0].Images[0])
-    res.render("search.ejs", { shops, userId }); // 검색 결과를 클라이언트에게 전달합니다.
-=======
 
     res.render("search.ejs", { shops, userId, name : user.name}); // 검색 결과를 클라이언트에게 전달합니다.
->>>>>>> df03e3b4ec164c5dd89b396cfb163f463cfe4c0d
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "검색 실패" });
